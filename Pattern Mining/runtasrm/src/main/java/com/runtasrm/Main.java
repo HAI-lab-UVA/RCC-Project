dc16bd9c6967002231167e5faa010a3c8ebe6eba
andbalch <andbalch@gmail.com>
2024-06-06 14:54:42 -0400

    Support for setting max ante. and cons. size

--- Pattern Mining/runtasrm/src/main/java/com/runtasrm/Main.java
+++ Pattern Mining/runtasrm/src/main/java/com/runtasrm/Main.java
@@ -30,6 +30,9 @@ public class Main {
         o.setRequired(true);
         options.addOption(o);
 
+        options.addOption(new Option("na", "numante", true, "max number of antecedents as an integer (> 0, >= length of xquery)"));
+        options.addOption(new Option("nc", "numcons", true, "max number of consequents as an integer (> 0, >= length of yquery)"));
+
         options.addOption(Option.builder("x")
                                 .longOpt("xquery")
                                 .desc("antecedents for query as integer tokens")
@@ -47,6 +50,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws ParseException {
+        // TODO: Support setting max ant. and cons. sizes
         Options options = makeOptions();
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
@@ -73,6 +77,26 @@ public class Main {
         }
 
         TaSRM_V3 tasrm = new TaSRM_V3();
+
+        if (cmd.hasOption("na")) {
+            int numAnte = Integer.parseInt(cmd.getOptionValue("na"));
+            if (numAnte >= xQuery.size() && numAnte > 0) {
+                tasrm.maxAntecedentSize = numAnte;
+            }
+            else {
+                System.out.println("Ignoring argument for numante, must be integer >= xQuery and > 0");
+            }
+        }
+        if (cmd.hasOption("nc")) {
+            int numCons = Integer.parseInt(cmd.getOptionValue("nc"));
+            if (numCons >= yQuery.size() && numCons > 0) {
+                tasrm.maxConsequentSize = numCons;
+            }
+            else {
+                System.out.println("Ignoring argument for numcons, must be integer >= yQuery and > 0");
+            }
+        }
+
         try {
             tasrm.runAlgorithm(minSupport, minConfidence, inputFP, outputFP, xQuery, yQuery);
         } catch (IOException e) {
